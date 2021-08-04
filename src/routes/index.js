b1cc7ec4c4e4f865f8487750865722cc38d7ee1b
Jorge Trujillo <88351609+MatiusRock1@users.noreply.github.com>
2021-08-04 15:28:51 -0500

    Visualizacion de datos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const task = require('../models/task');
 const router = express.Router();
+const Task = require('../models/task');
 
-router.get('/', (req, res) =>{
-    res.render('index');
+router.get('/', async (req, res) =>{
+    const tasks = await Task.find();
+       res.render('index' , {
+        tasks
+    });
 })
+
+router.post('/add', async (req,res) => {
+
+const task = new Task(req.body);
+await task.save();
+res.redirect('/');
+});
 
 module.exports = router;
