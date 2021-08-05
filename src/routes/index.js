ee804b2187355e8bbc446cf4083009f6bd421fc6
Jorge Trujillo <88351609+MatiusRock1@users.noreply.github.com>
2021-08-05 10:35:10 -0500

    Eliminar y cambiar de estado

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,4 +17,19 @@ await task.save();
 res.redirect('/');
 });
 
+router.get('/delete/:id' , async (req,res) => {
+    const { id } = req.params;
+    
+    await Task.remove({_id : id});
+    res.redirect('/');
+});
+router.get('/turn/:id' , async (req,res) => {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    task.status = !task.status;
+    await task.save();
+    res.redirect('/');
+});
+
+
 module.exports = router;
